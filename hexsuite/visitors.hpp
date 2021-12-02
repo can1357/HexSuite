3c5e0a51c496142cd560fb6826e341539fa2f109
Can Bölük <me@can.ac>
2021-12-02 14:15:01 +0300

    Implemented ctree visitors.

--- hexsuite/visitors.hpp
+++ hexsuite/visitors.hpp
@@ -36,4 +36,65 @@ namespace hex
 		operator minsn_visitor_t&() { return storage; }
 	};
 	template<typename F> minsn_visitor( F&& )->minsn_visitor<F>;
+
+	
+	// Ctree visitors:
+	//
+	namespace detail
+	{
+		template<typename F, typename... Tx>
+		concept CtreeCallableWith = requires( F && x ) { ( int ) x( std::declval<Tx>()... ); };
+	};
+	template<bool Pre, bool Post, typename F>
+	struct basic_ctree_visitor : ctree_visitor_t
+	{
+		F functor;
+		basic_ctree_visitor( int cv_flags, F&& functor ) : ctree_visitor_t( cv_flags ), functor( std::forward<F>( functor ) ) {}
+
+		int visit_insn( cinsn_t* e ) override 
+		{
+			if constexpr ( Pre && detail::CtreeCallableWith<F, ctree_visitor_t&, cinsn_t*> )
+				return functor( *this, e );
+			else if constexpr ( Pre && detail::CtreeCallableWith<F, cinsn_t*> )
+				return functor( e );
+			return 0; 
+		}
+		int visit_expr( cexpr_t* e ) override 
+		{
+			if constexpr ( Pre && detail::CtreeCallableWith<F, ctree_visitor_t&, cexpr_t*> )
+				return functor( *this, e );
+			else if constexpr ( Pre && detail::CtreeCallableWith<F, cexpr_t*> )
+				return functor( e );
+			return 0;
+		}
+		int leave_insn( cinsn_t* e ) override
+		{
+			if constexpr ( Pre && detail::CtreeCallableWith<F, ctree_visitor_t&, cinsn_t*> )
+				return functor( *this, e );
+			else if constexpr ( Pre && detail::CtreeCallableWith<F, cinsn_t*> )
+				return functor( e );
+			return 0;
+		}
+		int leave_expr( cexpr_t* e ) override
+		{
+			if constexpr ( Post && detail::CtreeCallableWith<F, ctree_visitor_t&, cexpr_t*> )
+				return functor( *this, e );
+			else if constexpr ( Post && detail::CtreeCallableWith<F, cexpr_t*> )
+				return functor( e );
+			return 0;
+		}
+	};
+	template<typename F>
+	struct ctree_pre_visitor : basic_ctree_visitor<true, false, F>
+	{
+		ctree_pre_visitor( F&& functor, int flags = CV_FAST ) : basic_ctree_visitor<true, false, F>( flags, std::forward<F>( functor ) ) {}
+	};
+	template<typename F> ctree_pre_visitor( F&& )->ctree_pre_visitor<F>;
+
+	template<typename F>
+	struct ctree_post_visitor : basic_ctree_visitor<false, true, F>
+	{
+		ctree_post_visitor( F&& functor, int flags = CV_FAST ) : basic_ctree_visitor<false, true, F>( flags | CV_POST, std::forward<F>( functor ) ) {}
+	};
+	template<typename F> ctree_post_visitor( F&& )->ctree_post_visitor<F>;
 };
